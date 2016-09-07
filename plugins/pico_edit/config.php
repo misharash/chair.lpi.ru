8297633d6adcdf68a6a13bd9bb06e62a188b426e
Rashkovetskyi Mykhailo <rashkovetskyi@notebook1.lpi.ru>
2016-09-07 19:16:20 +0300

    Added pico_edit plugin

---
+++ plugins/pico_edit/config.php
@@ -0,0 +1,11 @@
+<?php
+
+global $backend_password;
+
+/*
+ * This should be a sha1 hash of your password.
+ * Use a tool like http://www.sha1-online.com to generate.
+ */
+$backend_password = 'f40e834e27c10b3fdc24ef2acef792c0713ce324';
+
+?>
